7bd735d3b46660b92b1d6fc7d604c5654cc62b70
Булах Игорь <bulahigor@MacBook-Alla.local>
2019-11-24 13:32:24 +0300

    Added ssh key generation description to README.md

---
+++ secondary_index/CMakeFiles/Brown.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Brown"
+  "Brown.pdb"
+  "CMakeFiles/Brown.dir/secondary_index.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Brown.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
